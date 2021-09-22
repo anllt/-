340cdaa106c313da362fd497496e445b9fa2c514
anllt <91058615+anllt@users.noreply.github.com>
2021-09-22 22:29:17 +0800

    Add files via upload

---
+++ 找零.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+	const int AMOUNT=100;
+	int a=0;
+	printf("ÇëÊäÈë½ğ¶î:%d",a);
+	scanf("%d",&a);
+	int change=AMOUNT-a;
+	printf("ÕÒÄú%dÔª",change);
+	return 0;
+}
